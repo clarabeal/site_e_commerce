21a20735cde093e94f65ff76ab489171ce86ce7f
p2108003 <julian.degut@etu.univ-lyon1.fr>
2022-01-07 19:59:33 +0100

    Changement des textes et responsive

--- Vue/vueAccueil.php
+++ Vue/vueAccueil.php
@@ -10,15 +10,21 @@
         <?php endfor; ?>
         <button type="button" data-bs-target="#ourCarousel" data-bs-slide-to="3"></button>
       </div>
-      <div class="carousel-inner" style="height:600px;">
+      <div class="carousel-inner">
         <div class="carousel-item active">
-          <a href="index.php"><img src="Contenu/Images/tous_produits.jpg" style="height:1080px;width:1920px;">
-          <h1 class="text-uppercase text-center fw-bold" style="position:absolute; top:29%; left:50%; transform:translate(-50%,-71%); font-family: Serif; color: rgb(0,0,0); font-size: 50px;">TOUS NOS PRODUITS</h1></a>
+          <a href="index.php"><img src="Contenu/Images/tous_produits.jpg" style="width:100%;">
+          <div class="carousel-caption d-none d-md-block">
+            <h1 class="text-uppercase text-center fw-bold">TOUS NOS PRODUITS</h1>
+          </div>
+          </a>
         </div>
         <?php for($i = 0; $i < count($categories); $i++): ?>
           <div class="carousel-item">
-            <a href="<?="index.php?action=categorie&idCat=".$categories[$i]['id']?>" class="text-decoration-none"><img src="<?="Contenu/Images/".$categories[$i]['name'].".jpg"?>" class="d-block mx-auto" alt="<?=$categories[$i]['name']?>" style="height:1080px;width:1920px;">
-            <h1 class="text-uppercase text-center fw-bold" style="position:absolute; top:29%; left:50%; transform:translate(-50%,-71%); font-family: Serif; color: rgb(0,0,0); font-size: 50px;">NOS <?=$categories[$i]['name']?></h1></a>
+            <a href="<?="index.php?action=categorie&idCat=".$categories[$i]['id']?>" class="text-decoration-none"><img src="<?="Contenu/Images/".$categories[$i]['name'].".jpg"?>" class="d-block mx-auto" alt="<?=$categories[$i]['name']?>" style="width:100%;">
+            <div class="carousel-caption d-none d-md-block">
+              <h1 class="text-uppercase text-center fw-bold">NOS <?=$categories[$i]['name']?></h1>
+            </div>
+            </a>
           </div>
         <?php endfor; ?>
       </div>
@@ -35,7 +41,7 @@
 </div>
 
 <div class="row">
-  <div class="col-10 bg-light mx-auto mt-3" style="border-radius: 12px;">
+  <div class="col-11 bg-light mx-auto mt-3" style="border-radius: 12px;">
   <?php for($i = 0; $i < count($produits); $i = $i + 2): ?>
     <div class="row">
       <div class="col d-flex justify-content-center text-center m-5">

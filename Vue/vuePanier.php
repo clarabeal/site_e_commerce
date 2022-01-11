114297d2220570b67d4ca194bb9dbb284d5bd28a
p1907112 <clara.beal@etu.univ-lyon1.fr>
2022-01-11 16:23:10 +0100

    Pareil

--- Vue/vuePanier.php
+++ Vue/vuePanier.php
@@ -13,22 +13,25 @@ $total=0;
 
 <!-- Pas de $produit si pas connecté -->
 <?php if(isset($produits)):?>
-<?php foreach($produits as $produit): ?>
-  <img src="<?= "Contenu/Images/".$produit['image']?>">
-  <p><?=$produit['name']?></p></a>
-  <p>Prix : <?=$produit['price']; ?> €</p>
-  <p>Quantité : <?=$produit['quantity']; ?></p>
+  <?php foreach($produits as $produit): ?>
+    <img src="<?= "Contenu/Images/".$produit['image']?>">
+    <p><?=$produit['name']?></p></a>
+    <p>Prix : <?=$produit['price']; ?> €</p>
+    <p>Quantité : <?=$produit['quantity']; ?></p>
 
-  <?php 
-    $prix=$produit['price'];
-    $quantite=$produit['quantity'];
-    $total=$total + $prix*$quantite;
-  ?>
+    <?php 
+      $prix=$produit['price'];
+      $quantite=$produit['quantity'];
+      $total=$total+$prix*$quantite;
+    ?>
 
-<?php endforeach ?>
-<?php endif ?>
+  <?php endforeach ?>
+  
 
-<?php if($commande): ?>
-  <p>Prix total de la commande : <?=$total?> €</p>
-  <a href="index.php?action=saisirAdresse" class="buttn">Passer la commande</a>
+  <!-- On affiche le total et le bouton pour passer la commande seulement si 'lutilisateur a une commande en cours -->
+  <?php if($commande): ?>
+    <p>Prix total de la commande : <?=$total?> €</p>
+    <a href="index.php?action=saisirAdresse" class="buttn">Passer la commande</a>
+  <?php endif ?>
+
 <?php endif ?>
